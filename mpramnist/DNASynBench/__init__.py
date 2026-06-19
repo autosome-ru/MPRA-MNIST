49d03fdc01d0bc3e6a87acddf1a4724802b0ca4a
VadimStrek <119708055+VadimStrek@users.noreply.github.com>
2026-06-19 23:49:12 +0300

    Update __init__.py

--- mpramnist/DNASynBench/__init__.py
+++ mpramnist/DNASynBench/__init__.py
@@ -1,4 +1,4 @@
 from .dataset import SimpleMotifDataset, LinCoopDataset, NonlinCoopDataset, AlienDataset, CombinationDataset, DistanceDataset
-from .trainer import LitModel_DNASyn
+from .trainer import LitModel_DNASyn_REG, LitModel_DNASyn_CLS
 
-__all__ = ['SimpleMotifDataset', 'LinCoopDataset', 'NonlinCoopDataset', 'AlienDataset', 'CombinationDataset', 'DistanceDataset', 'LitModel_DNASyn']
+__all__ = ['SimpleMotifDataset', 'LinCoopDataset', 'NonlinCoopDataset', 'AlienDataset', 'CombinationDataset', 'DistanceDataset', 'LitModel_DNASyn_REG', 'LitModel_DNASyn_CLS']
