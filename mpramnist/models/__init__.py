76629b42a9e60de3857d8e98e3252f69ef4a4552
Nikita P <nios.583@gmail.com>
2026-06-22 18:57:52 +0300

    add dream-rnn model

--- mpramnist/models/__init__.py
+++ mpramnist/models/__init__.py
@@ -2,5 +2,7 @@ from .MPRALegNet import HumanLegNet, initialize_weights
 from .MPRAnn import MPRAnn
 from .PARM import PARM
 from .Malinois import L1KLmixed, BassetBranched
-from.DeepStarr import DeepStarr
-__all__ = ['HumanLegNet', 'initialize_weights', 'MPRAnn', 'PARM', 'L1KLmixed', 'BassetBranched', 'DeepStarr']
+from .DeepStarr import DeepStarr
+from .DREAM_RNN import DREAM_RNN, BHIFirstLayersBlock, BHICoreBlock, AutosomeFinalLayersBlock
+__all__ = ['HumanLegNet', 'initialize_weights', 'MPRAnn', 'PARM', 'L1KLmixed', 'BassetBranched', 'DeepStarr',
+           'DREAM_RNN', 'BHIFirstLayersBlock', 'BHICoreBlock', 'AutosomeFinalLayersBlock']
