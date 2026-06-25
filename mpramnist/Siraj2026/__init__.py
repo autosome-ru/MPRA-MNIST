a7def6a05041f883b99f81a623cd78c56208b29f
Vladislav Labanov <labanovvlad3125@gmail.com>
2026-06-25 14:56:14 +0000

    SirajMPRADataset beta

--- mpramnist/Siraj2026/__init__.py
+++ mpramnist/Siraj2026/__init__.py
@@ -1,4 +1,4 @@
-from .dataset import SirajDataset
+from .dataset import SirajMPRADataset, SirajSATMUTDataset
 from .trainer import LitModel_Siraj
 
 __all__ = ["SirajMPRADataset", "SirajSATMUTDataset", "LitModel_Siraj"]
