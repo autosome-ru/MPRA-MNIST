f9913b5119a4a99833afb5ddd11d3f52f4509fe3
VadimStrek <119708055+VadimStrek@users.noreply.github.com>
2026-07-22 16:53:37 +0300

    Add OrderedDataset

--- mpramnist/DNASynBench/__init__.py
+++ mpramnist/DNASynBench/__init__.py
@@ -1,4 +1,5 @@
-from .dataset import SimpleMotifDataset, LinCoopDataset, NonlinCoopDataset, AlienDataset, CombinationDataset, DistanceDataset
+from .dataset import SimpleMotifDataset, LinCoopDataset, NonlinCoopDataset, AlienDataset, CombinationDataset, DistanceDataset, OrderedDataset
 from .trainer import LitModel_DNASyn_REG, LitModel_DNASyn_CLS
 
-__all__ = ['SimpleMotifDataset', 'LinCoopDataset', 'NonlinCoopDataset', 'AlienDataset', 'CombinationDataset', 'DistanceDataset', 'LitModel_DNASyn_REG', 'LitModel_DNASyn_CLS']
+__all__ = ['SimpleMotifDataset', 'LinCoopDataset', 'NonlinCoopDataset', 'AlienDataset', 'CombinationDataset',
+           'DistanceDataset', 'OrderedDataset', 'LitModel_DNASyn_REG', 'LitModel_DNASyn_CLS']
