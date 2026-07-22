b38c30ad945259054e18428199219956f5e85aa5
VadimStrek <119708055+VadimStrek@users.noreply.github.com>
2026-07-22 16:47:59 +0300

    add models

--- mpramnist/models/__init__.py
+++ mpramnist/models/__init__.py
@@ -2,5 +2,12 @@ from .MPRALegNet import HumanLegNet, initialize_weights
 from .MPRAnn import MPRAnn
 from .PARM import PARM
 from .Malinois import L1KLmixed, BassetBranched
-from.DeepStarr import DeepStarr
-__all__ = ['HumanLegNet', 'initialize_weights', 'MPRAnn', 'PARM', 'L1KLmixed', 'BassetBranched', 'DeepStarr']
+from .DeepStarr import DeepStarr
+from .CNN import CNN
+from .DREAM_RNN import DREAM_RNN
+from .BHI import BHI
+from .DREAM_Attn import DREAM_Attn
+from .GENA_LM import GENA_LM
+from .NucTrans import NucTrans
+__all__ = ['HumanLegNet', 'initialize_weights', 'MPRAnn', 'PARM', 'L1KLmixed','BassetBranched',
+           'DeepStarr', 'CNN', 'DREAM_RNN', 'BHI', 'DREAM_Attn', 'GENA_LM', 'NucTrans']
